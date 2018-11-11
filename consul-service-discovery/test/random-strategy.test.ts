3838a93b6c63d0989fd1ccb9193330ba022b3c8a
rnkoaa <richard.agyei@gmail.com>
2018-11-10 22:22:56 -0600

    test random selection strategy

---
+++ consul-service-discovery/test/random-strategy.test.ts
@@ -0,0 +1,63 @@
+import { TestInstanceProvider } from './test-instance-provider';
+import { DataStore } from '../lib/data-store';
+
+import { SelectionStrategyFactory } from '../lib/selection-strategy';
+const selectionStrategyFactory = new SelectionStrategyFactory();
+
+test('random selection strategy for a single instance', () => {
+  const dataStore: DataStore = new DataStore();
+  const instance = TestInstanceProvider.generateOneInstance();
+  expect(instance).not.toBeUndefined;
+  expect(instance.serviceId).toEqual('service-2');
+  expect(instance.serviceName).toEqual('service-name-2');
+
+  dataStore.addInstance(instance);
+  const randomSelectionStrategy = selectionStrategyFactory.createStrategy('random');
+  const serviceInstance = randomSelectionStrategy.select('service-name-2', dataStore);
+  expect(serviceInstance).not.toBeUndefined;
+  expect(serviceInstance.serviceId).toEqual('service-2');
+  expect(serviceInstance.serviceName).toEqual('service-name-2');
+});
+
+test('random selection strategy for a non existent instance', () => {
+  const dataStore: DataStore = new DataStore();
+  const instance = TestInstanceProvider.generateOneInstance();
+  expect(instance).not.toBeUndefined;
+  expect(instance.serviceId).toEqual('service-2');
+  expect(instance.serviceName).toEqual('service-name-2');
+
+  dataStore.addInstance(instance);
+  const randomSelectionStrategy = selectionStrategyFactory.createStrategy('random');
+  const serviceInstance = randomSelectionStrategy.select('service-non-existent', dataStore);
+  expect(serviceInstance).not.toBeUndefined;
+  expect(serviceInstance.serviceId).toBeUndefined;
+  expect(serviceInstance.serviceName).toBeUndefined;
+});
+
+test('random selection strategy for multiple instances', () => {
+  const dataStore: DataStore = new DataStore();
+  const count = 4;
+  const instances = TestInstanceProvider.multipleInstancesOfSameService('service-name', count);
+  dataStore.addInstances(instances);
+
+  const randomSelectionStrategy = selectionStrategyFactory.createStrategy('random');
+  const serviceInstance = randomSelectionStrategy.select(`service-name-${count}`, dataStore);
+  expect(serviceInstance).not.toBeUndefined;
+  expect(serviceInstance.serviceName).toEqual(`service-name-${count}`);
+  expect(serviceInstance.host).toEqual(`localhost-${serviceInstance.id}`);
+});
+
+test('random selection strategy for multiple instances and multiple services', () => {
+  const dataStore: DataStore = new DataStore();
+  const count = 4;
+  const instances = TestInstanceProvider.multipleInstancesOfSameService('service-name', count);
+  dataStore.addInstances(instances);
+  const cubeInstances = TestInstanceProvider.multipleInstancesOfSameService('service-cube', count);
+  dataStore.addInstances(cubeInstances);
+
+  const randomSelectionStrategy = selectionStrategyFactory.createStrategy('random');
+  const serviceInstance = randomSelectionStrategy.select(`service-name-${count}`, dataStore);
+  expect(serviceInstance).not.toBeUndefined;
+  expect(serviceInstance.serviceName).toEqual(`service-name-${count}`);
+  expect(serviceInstance.host).toEqual(`localhost-${serviceInstance.id}`);
+});
